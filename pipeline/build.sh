bc3f22e9a3fa089b2f8a5146b1c66fa45757fbdc
maciej <maciekursyn@gmail.com>
2026-01-19 18:44:47 +0100

    fixed release pipeline

--- pipeline/build.sh
+++ pipeline/build.sh
@@ -16,19 +16,6 @@ export CGO_ENABLED=0
 export GOOS=linux
 export GOARCH=amd64
 
-# optional metadata
-GIT_COMMIT=$(git rev-parse --short HEAD)
-GIT_TAG=$(git describe --tags --dirty --always 2>/dev/null || echo "dev")
-BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-
-# build flags
-LDFLAGS=(
-  "-s -w"
-  "-X main.version=$GIT_TAG"
-  "-X main.commit=$GIT_COMMIT"
-  "-X main.date=$BUILD_DATE"
-)
-
 go build \
   -trimpath \
   -ldflags "${LDFLAGS[*]}" \
